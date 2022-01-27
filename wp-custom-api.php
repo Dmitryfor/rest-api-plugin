eeacf12d545247f83bc38c14f7a0ac2ee1a26d4f
Dmitryfor <dimaportnov2000@gmail.com>
2022-01-27 20:10:42 +0200

    refactore code

--- wp-custom-api.php
+++ wp-custom-api.php
@@ -24,41 +24,17 @@ add_filter('request', function($vars) {
 
 });
 
-add_filter('template_redirect', function($template) {
+add_action('template_redirect', function($template) {
 
-	if ( ( is_singular() || is_page() ) && ( get_query_var('json') ) ) {
+	if ( is_singular( [ 'post', 'page' ] ) && ( get_query_var('json') ) ) {
 
-        $post = get_queried_object();
-        $password_value = ( !empty( $post->post_password ) ) ? true : false;
-        
-		$response = [
-            'id'            => $post->ID,
-            'date'          => $post->post_date,  
-            'date_gmt'      => $post->post_date_gmt,
-            'guid'          => [
-                'rendered'  => $post->guid
-            ],
-            'slug'          => $post->post_name, 
-            'status'        => $post->post_status,
-            'type'          => $post->post_type,   
-            'title'         => [
-                'rendered'  => $post->post_title
-            ], 
-            'content'       => [
-                'rendered'  => strip_tags( $post->post_content ),
-                'protected' => $password_value,
-            ],
-            'excerpt'       => [
-                'rendered'  => strip_tags( $post->post_excerpt ),
-                'protected' => $password_value,
-            ],
-            'author'        => $post->post_author,  
-            #etc.
-        ];
+        $post = get_queried_object();        
+        $post_type = $post->post_type == 'post' ? 'posts' : 'pages';
+		$request = new WP_REST_Request( 'GET', "/wp/v2/{$post_type}/$post->ID" );
+        $response = rest_do_request( $request );
+        $data = rest_get_server()->response_to_data( $response, false );
 
-        header('Content-Type: text/plain');
-        echo json_encode( $response );
-
+        wp_send_json($data);
         exit();
         
 	}
